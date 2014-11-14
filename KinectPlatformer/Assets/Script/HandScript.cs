8513d7bc2e7ab83225c6d6554bc471bf58551caa
grecep <gregor.cepin@gmail.com>
2014-11-14 17:55:06 +0100

    realistic hands that hit

--- KinectPlatformer/Assets/Script/HandScript.cs
+++ KinectPlatformer/Assets/Script/HandScript.cs
@@ -14,11 +14,12 @@ public class HandScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-				Vector3 pos1 = elbow.transform.position;
-				Vector3 pos2 = wrist.transform.position;
+		Vector3 pos1 = elbow.transform.position;
+		Vector3 pos2 = wrist.transform.position;
 
-				transform.position = pos1;
-				this.transform.LookAt (pos2);
+		transform.position = pos1;
+		this.transform.LookAt (pos2);
+		this.transform.Rotate (0, 90, 0);
 	}
 	void OnCollisionEnter (Collision col) {	
 		if (col.gameObject.name == "OurCylinder") {
