2df5a7707e528d0058054bb823167617b03e4212
unknown <ian.r.buck@gmail.com>
2014-12-03 15:40:37 +0100

    Laid groudwork for detecting rapid head movement.

--- KinectPlatformer/Assets/CameraScript.cs
+++ KinectPlatformer/Assets/CameraScript.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 	public GameObject head;
+	public static int delay = 10;
+	public Vector3[] pastOrientations = new Vector3[delay];
+	public int currentIndex = 0;
+	public Vector3 currentOrientation;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +15,19 @@ public class CameraScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = head.transform.position - (transform.forward * 2);
+		currentOrientation = this.transform.FindChild("OVRCameraController").FindChild("CameraLeft").transform.eulerAngles;
+		pastOrientations [currentIndex] = currentOrientation;
+		currentIndex++;
+		if (currentIndex >= delay) {
+			currentIndex -= delay;
+		}
+		if (pastOrientations [currentIndex].y - currentOrientation.y > 45
+			|| currentOrientation.y - pastOrientations [currentIndex].y > 45) {
+			print ("Rapid horizontal movement detected");
+		}
+		if (pastOrientations [currentIndex].x - currentOrientation.x > 45
+			|| currentOrientation.x - pastOrientations [currentIndex].x > 45) {
+			print ("Rapid vertical movement detected.");
+		}
 	}
 }
